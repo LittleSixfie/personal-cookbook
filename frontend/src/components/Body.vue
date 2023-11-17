8edaa05b42292001a9bed6555808f1222d5fff26
LittleSixfie <jorge-padron99@hotmail.com>
2023-11-17 09:41:19 +0100

    Separando por componentes para que no sea caotico

--- frontend/src/components/Body.vue
+++ frontend/src/components/Body.vue
@@ -1,33 +1,17 @@
 <template>
-    <h1>Body</h1>
-    <v-main>
-      
+    <DialogRecipe></DialogRecipe>
+    <v-main>      
       <v-row justify="center">
-        <v-col
-          cols="11"
-          
-        >
-          <v-sheet
-            elevation="10"
-            rounded="xl"
-          >
-            <v-sheet
-              class="pa-3 bg-primary text-right"
-              rounded="t-xl"
-            >
+        <v-col cols="11">
+          <v-sheet elevation="10" rounded="xl" >
+            <v-sheet class="pa-3 bg-primary text-right" rounded="t-xl">
               <v-btn @click="burron" icon >
                 <v-icon>mdi-filter-check</v-icon>
               </v-btn>
-
-              <v-btn
-                class="ms-2"
-                icon
-                @click="reset"
-              >
+              <v-btn class="ms-2" icon @click="reset">
                 <v-icon>mdi-reload</v-icon>
               </v-btn>
             </v-sheet>
-
             <div class="pa-4">
               <v-chip-group
                 cols="12"
@@ -48,91 +32,77 @@
       <v-container>
         <v-row>
           <v-col
-            v-for="n in recipes"
-            :key="n"
+            v-for="recipe in recipes"
+            :key="recipe"
             cols="4"
           >
-            <v-card height="200"
-              :title="n.title_name"
-            >
-            <v-card-text class="d-inline-block text-overflow-ellipses" >{{n.instructions}} </v-card-text>
-            </v-card>
+          <CardRecipe :recipe="recipe" ></CardRecipe>
           </v-col>
         </v-row>
       </v-container>
-      <!--
-      <v-list lines="one">
-        <v-list-item
-            v-for="post in posts"
-            :key="post"
-            :title="'Item ' + post.id"
-            :subtitle='post.title_name + post.nombre'
-        ></v-list-item>
-        
-      </v-list>
-      -->
     </v-main>
 </template>
 
 <script>
-    import axios from "axios";
-import { toHandlers } from "vue";
-    import { ref } from 'vue'
-    
-    export default {
-        name: 'Body',
-        methods: {
-          async burron() {
-            try {
-              //TODO: Multivalue aplicar
-              const newRecipes=[]
-              for(const chip of this.chipFilter) {
-                const response = await axios.get(`http://localhost:3000/ingridientsList/ingridient/${this.ingridients[chip].id}`)
-                console.log("response", response)
-                for(const idReceta of response.data){
-                  console.log(idReceta)
-                  newRecipes.push(this.recipes.find(element => element.id == idReceta.idreceta))
-                }
-                console.log("vuelta nuevas recetas", newRecipes)
-              }
-              this.recipes = newRecipes
-            } catch (e) {
-              alert(e)
+  import axios from "axios";
+  import CardRecipe from "./CardRecipe.vue";
+  import DialogRecipe from "./DialogRecipe.vue"
+  export default {
+    name: "Body",
+    methods: {
+      async burron() {
+        try {
+          //TODO: Multivalue aplicar
+          const newRecipes = [];
+          for (const chip of this.chipFilter) {
+            const response = await axios.get(`http://localhost:3000/ingridientsList/ingridient/${this.ingridients[chip].id}`);
+            console.log("response", response);
+            for (const idReceta of response.data) {
+              console.log(idReceta);
+              newRecipes.push(this.recipes.find(element => element.id == idReceta.idreceta));
             }
-          },
-          async reset(){
-            try {
-              const responseAllRecipes = await axios.get(`http://localhost:3000/recipes`)
-              this.recipes = responseAllRecipes.data
-              this.chipFilter = []
-            } catch (e) {
-              alert(e.response)
-              this.errors.push(e)
-            }
+            console.log("vuelta nuevas recetas", newRecipes);
           }
-        },
-        data() {
-            return {
-              recipes: [],
-              ingridients: [],
-              chipFilter: [],
-              errors: [],
-              counter: 0,
-            }
-          },
-          // Fetches posts when the component is created.
-          async created() {
-            try {
-              const responseAllRecipes = await axios.get(`http://localhost:3000/recipes`)
-              this.recipes = responseAllRecipes.data
-              const responseAllIngridients = await axios.get(`http://localhost:3000/ingridients`)
-              this.ingridients = responseAllIngridients.data
-            } catch (e) {
-              alert(e.response)
-              this.errors.push(e)
-            }
-          },
-        
+          this.recipes = newRecipes;
+        } catch (e) {
+          alert(e);
+        }
+      },
+      async reset() {
+        try {
+          const responseAllRecipes = await axios.get(`http://localhost:3000/recipes`);
+          this.recipes = responseAllRecipes.data;
+          this.chipFilter = [];
+        } catch (e) {
+          alert(e.response);
+          this.errors.push(e);
+        }
+      }
+    },
+    data() {
+      return {
+        recipes: [],
+        ingridients: [],
+        chipFilter: [],
+        errors: [],
+      };
+    },
+    // Fetches posts when the component is created.
+    async created() {
+      try {
+        const responseAllRecipes = await axios.get(`http://localhost:3000/recipes`);
+        this.recipes = responseAllRecipes.data;
+        const responseAllIngridients = await axios.get(`http://localhost:3000/ingridients`);
+        this.ingridients = responseAllIngridients.data;
+        console.log(this.recipes)
+      } catch (e) {
+        alert(e.response);
+        this.errors.push(e);
+      }
+    },
+    components: {
+      CardRecipe,
+      DialogRecipe
     }
+  }
 </script>
-
---
+++ frontend/src/components/CardRecipe.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-card height="200">
+        <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="110px"
+            cover
+        ></v-img>
+        <v-card-title> 
+            {{recipe.title_name}}
+        </v-card-title>
+        <v-card-actions>
+            <v-btn
+                class="ms-2"
+                @click=""
+                color="orange-lighten-2"
+                variant="text"
+            >
+                Receta completa
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    import DialogRecipe from './DialogRecipe.vue';
+    export default {
+        name: 'CardRecipe',
+        props: ['recipe'],
+        components:{
+            DialogRecipe
+        }
+    }
+</script>
